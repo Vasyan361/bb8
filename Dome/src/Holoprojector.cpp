1178619d717fae8d041af7d2424871c8767ff6b3
Vasyan361 <vasyagromak@yandex.ru>
2023-06-28 01:41:39 +0300

    Finish holoprojector blink logic

--- Dome/src/Holoprojector.cpp
+++ Dome/src/Holoprojector.cpp
@@ -5,9 +5,114 @@ void Holoprojector::init()
     Holoprojector::pixel.begin();
 }
 
-void Holoprojector::dissable()
+void Holoprojector::disable()
 {
     Holoprojector::pixel.clear();
     Holoprojector::pixel.setPixelColor(0, Holoprojector::pixel.Color(0, 0, 0));
+    Holoprojector::pixel.show();
+}
+
+void Holoprojector::pulse()
+{
+    if(Holoprojector::pulseState == 0 && Holoprojector::blue <= 100) {
+        Holoprojector::blue++;
+    }
+    
+    if(Holoprojector::blue >= 100) {
+        Holoprojector::pulseState = 1;
+    }
+  
+    if(Holoprojector::pulseState == 1 && Holoprojector::blue >= 10){
+      Holoprojector::blue--;
+    }
+    
+    if(Holoprojector::blue <= 10) {
+        Holoprojector::pulseState = 0;
+    }
+    
+    Holoprojector::pixel.setPixelColor(0, Holoprojector::pixel.Color(0, 0, Holoprojector::blue));
+    Holoprojector::pixel.show();
+}
+
+void Holoprojector::cycle()
+{
+    switch (Holoprojector::cycleState)
+    {
+    case 0:
+        Holoprojector::red = 0;
+        Holoprojector::green = 0;
+        Holoprojector::blue = 0;
+        Holoprojector::cycleState = 1;
+        break;
+    case 1:
+        if(Holoprojector::red <= 250) {
+            Holoprojector::red += 3;
+        } else {
+            Holoprojector::cycleState = 2;
+        }
+        break;
+    case 2:
+        if(Holoprojector::red >= 10) {
+            Holoprojector::red -= 3;
+        } else {
+            Holoprojector::cycleState = 3;
+        }
+        break;
+    case 3:
+        if(Holoprojector::green <= 250) {
+            Holoprojector::green += 3;
+        } else {
+            Holoprojector::cycleState = 4;
+        }
+        break;
+    case 4:
+        if(Holoprojector::green >= 10) {
+            Holoprojector::green -= 3;
+        } else {
+            Holoprojector::cycleState = 5;
+        }
+        break;
+    case 5:
+        if(Holoprojector::blue <= 250) {
+            Holoprojector::blue += 3;
+        } else {
+            Holoprojector::cycleState = 6;
+        }
+        break;
+    case 6:
+        if(Holoprojector::blue >= 10) {
+            Holoprojector::blue -= 3;
+        } else {
+            Holoprojector::cycleState = 7;
+        }
+        break;
+    case 7:
+        if(Holoprojector::red <= 250) {
+            Holoprojector::red += 3;
+        } else if(Holoprojector::green <= 250) {
+            Holoprojector::green += 3;
+        } else if(Holoprojector::blue <= 250) {
+            Holoprojector::blue += 3;
+        } else {
+            Holoprojector::cycleState = 8;
+        }
+        break;
+    case 8:
+        if(Holoprojector::red >= 10) {
+            Holoprojector::red -= 3;
+        } else if(Holoprojector::green >= 10) {
+            Holoprojector::green -= 3;
+        } else if(Holoprojector::blue >= 10) {
+            Holoprojector::blue -= 3;
+        } else {
+            Holoprojector::cycleState = 1;
+        }
+        break;
+    
+    default:
+        break;
+    }
+  
+    Holoprojector::pixel.setPixelColor(0, Holoprojector::pixel.Color(Holoprojector::red, Holoprojector::green, Holoprojector::blue));
     Holoprojector::pixel.show();
 }
--- Dome/src/Holoprojector.h
+++ Dome/src/Holoprojector.h
@@ -8,6 +8,14 @@ class Holoprojector
 {
     public:
         void init();
+        void disable();
+        void pulse();
+        void cycle();
     private:
         Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, HOLOPROJECTOR_PIN, NEO_GRB + NEO_KHZ800);
+        int pulseState = 0;
+        int cycleState = 0;
+        int red = 0;
+        int green = 0;
+        int blue = 0;
 };
